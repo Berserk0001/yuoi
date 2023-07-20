6a2fd93af61510355a3a7915726a9f6851ea72bb
J Santos <123162572+jonosantoso@users.noreply.github.com>
2023-07-20 21:02:26 +0700

    Update compress.js
    experimental avif implementation, not used for now

--- util/compress.js
+++ util/compress.js
@@ -1,19 +1,38 @@
 const sharp = require("sharp");
 
 function compress(input, webp, grayscale, quality, originSize, metadata) {
-	let format = webp ? "webp" : "jpeg";
+	let format = webp ? 'webp' : 'jpeg';
 	let imgWidth = metadata.width;
 	let imgHeight = metadata.height;
 	let compressionQuality = quality;
+	let effortCPU = 0;
 
 	//workaround for webp max res limit
 	if (imgWidth >= 16383 || imgHeight >= 16383) {
 		format = 'jpeg';
 		compressionQuality *= 2.5;
+		effortCPU = 0;
 	} else {
 		format = 'webp';
 		compressionQuality *= 0.5;
+		effortCPU = 6;
 	}
+
+	/* experimental avif compression
+	if (imgWidth >= 16383 || imgHeight >= 16383) {
+	  format = 'jpeg';
+	  compressionQuality *= 2.5;
+	  effortCPU = 0;
+	} else if (imgWidth <= 8704 && imgHeight <= 8704) {
+	  format = 'avif';
+	  compressionQuality *= 2.0;
+	  effortCPU = 2;
+	} else if (imgWidth <= 16383 || imgHeight <= 16383) {
+	  format = 'webp';
+	  compressionQuality *= 0.5;
+	  effortCPU = 6;
+	}
+ 	*/
 	
         quality = Math.ceil(compressionQuality)
 	
@@ -22,7 +41,7 @@ function compress(input, webp, grayscale, quality, originSize, metadata) {
 		.toFormat(format, {
 			quality: quality,
 			mozjpeg: true,
-			effort: 6
+			effort: effortCPU
 		})
 		.toBuffer({resolveWithObject: true})
 		.then(({data: output, info}) => {	// this way we can also get the info about output image, like height, width
