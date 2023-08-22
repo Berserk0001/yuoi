ba9c6d4bad54934d159404c2e0921641c9e2b452
J Santos <123162572+jonosantoso@users.noreply.github.com>
2023-08-22 16:56:28 +0700

    Update compress.js
    use resizing logic for longstrip image

--- util/compress.js
+++ util/compress.js
@@ -1,21 +1,24 @@
-const sharp = require("sharp");
+const sharp = require("sharp")
 
 function compress(input, webp, grayscale, quality, originSize, metadata) {
-	let format = webp ? 'webp' : 'jpeg';
-	let imgWidth = metadata.width;
-	let imgHeight = metadata.height;
-	let compressionQuality = quality;
-	let effortCPU = 0;
+	let format = webp ? 'webp' : 'jpeg'
+	let imgWidth = metadata.width
+	let imgHeight = metadata.height
+	let compressionQuality = quality
+	let effortCPU = 2
+	let resizeWidth = null
+	let resizeHeight = null
 
-	//workaround for webp max res limit
-	if (imgWidth >= 16383 || imgHeight >= 16383) {
-		format = 'jpeg';
-		compressionQuality *= 2.5;
-		effortCPU = 0;
+	//workaround for webp max res limit by resizing
+	if (imgHeight >= 16383) {	//longstrip webtoon/manhwa/manhua
+		format = 'webp'
+		compressionQuality *= 0.5
+		effortCPU = 4
+		resizeHeight = 15383
 	} else {
-		format = 'webp';
-		compressionQuality *= 0.5;
-		effortCPU = 6;
+		format = 'webp'
+		compressionQuality *= 0.5
+		effortCPU = 6
 	}
 
 	/* experimental avif compression
@@ -34,13 +37,17 @@ function compress(input, webp, grayscale, quality, originSize, metadata) {
 	}
  	*/
 	
-        quality = Math.ceil(compressionQuality)
+        //quality = Math.ceil(compressionQuality)
 	
 	return sharp(input)
+		.resize({
+			width: resizeWidth,
+			height: resizeHeight
+		})
 		.grayscale(grayscale)
 		.toFormat(format, {
 			quality: quality,
-			mozjpeg: true,
+			preset: 'picture',
 			effort: effortCPU
 		})
 		.toBuffer({resolveWithObject: true})
