b1faccbe9072eb36d34354deeb0821abff373316
Rushs321 <fyggh566@tutamail.com>
2024-11-14 20:52:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-// index.js or app.js
-const app = require('./server');  // Import the Express app from server.js
+#!/usr/bin/env node
+'use strict';
+const express = require('express');
+const app = express();
 
+const proxy = require('./functions/index.js'); // Import the proxy handler from index.js
 const PORT = process.env.PORT || 3000;
+
+app.enable('trust proxy');  // Enable trust proxy if behind a proxy
+
+// Define routes
+app.get('/', proxy);  // Use proxy handler for root route
+app.get('/favicon.ico', (req, res) => res.status(204).end());  // Favicon route
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
